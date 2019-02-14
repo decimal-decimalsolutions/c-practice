be9b9dccf025536a925f519c26f9fa7aa45a5b3f
Pankaj <pkchopra@gmail.com>
2019-02-13 22:42:56 -0600

    your update

--- potd-q16/potd.cpp
+++ potd-q16/potd.cpp
@@ -1,6 +1,31 @@
 #include "potd.h"
 #include <iostream>
 
-void insertSorted(Node **head, Node *insert) {
-  // your code here!
+void insertSorted(Node **head, Node *insert)
+{
+//    std::cout<<insert->data_<< std::endl;
+    Node *ptr2Head = *head;
+
+//    Node *ptr2Head = *head;
+    if (*head == NULL) {
+        *head = insert;
+    } else if (ptr2Head->data_ >= insert->data_) {
+//        std::cout<< ptr2Head->data_<<insert->data_<< std::endl;
+        insert->next_ = ptr2Head;
+        ptr2Head = insert;
+    } else {
+        // we have already compared head lets start with next
+        //we need track one previous and one next node
+        // it is better to have previous node and point to next with cptr->next
+        //so cPtr is previous and cPtr->next is next
+        Node *cPtr = ptr2Head;
+        //comparing second with new node with second node
+        while (cPtr->next_ != NULL && cPtr->next_->data_ <= insert->data_) {
+            cPtr = cPtr->next_;
+        }
+// Now we are pointing at the node that is greater than new node
+        insert->next_ = cPtr->next_;
+        cPtr->next_ = insert;
+    }
+
 }
--- potd-q17/Node.cpp
+++ potd-q17/Node.cpp
@@ -1,37 +1,59 @@
 #include "Node.h"
+#include <iostream>
+using namespace std;
 
 void sortList(Node **head) {
-  // your code here!
+    // your code here!
 
-  /*
-  Selection sort:
-  Repeatedly moving the minimum node
-  to the beginning
-  */
+    /*
+    Selection sort:
+    Repeatedly moving the minimum node
+    to the beginning
+    */
 
 
-  // Some variables you probably will need
-  Node *minNode = NULL;
-  Node *minPrev = NULL;
+    // Some variables you probably will need
+    Node *minNode = NULL;
+    Node *minPrev = NULL;
 
-  if (*head == NULL)
-      return;
+    if (*head == NULL)
+        return;
 
-  int minData = (*head)->data_;
-  Node *tmp = *head;
-  Node *prev = NULL;
+    int minData = (*head)->data_;
+    Node *tmp = *head;
+    Node *prev = NULL;
+    minNode = tmp;
+//    std::cout<<"before MinData is "<<tmp->data_<< std::endl;
+    while ( tmp != NULL ) {
+        // TODO: Find the minimum node
 
-  while ( tmp != NULL ) {
-      // TODO: Find the minimum node
-  }
+//        std::cout << "In while Loop  " << tmp->data_ << std::endl;
+        //comparing second with new node with second node
+        if (minData > tmp->data_) {
+            minNode = tmp;
+            minData = minNode->data_;
+            minPrev = prev;
+            prev = tmp;
+            tmp = tmp->next_;
+//            if(tmp!= NULL)
+//             std::cout <<"minData is " <<minData<< " In If Now tmp is " << tmp->data_ << std::endl;
+        } else {
+            prev = tmp;
+            tmp=tmp->next_;
+//            if(tmp!= NULL)
+//                std::cout << "minData is " <<minData<< " In else  Now tmp is " << tmp->data_ << std::endl;
+        }
+    }
+//    std::cout<<" After MinData is  "<<minNode->data_<< std::endl;
+    if (minPrev != NULL && minNode != NULL) {
+        // TODO: Move the minimum node to the beginning
+      minPrev->next_ = minNode->next_;
+      minNode->next_=*head;
+     *head = minNode;
+    }
 
-  if (minPrev != NULL && minNode != NULL) {
-      // TODO: Move the minimum node to the beginning
-      // and update head
-  }
-
-  // Recurse on the rest of the list
-  sortList(& (*head)->next_);
+    // Recurse on the rest of the list
+    sortList(& (*head)->next_);
 }
 
 Node::Node() {
